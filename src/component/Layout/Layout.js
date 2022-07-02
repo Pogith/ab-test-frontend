a54dcab2e235dd8ca6b8faad39e5daa6964db7b1
chlugsdh58 <chlgusdh58@gmail.com>
2022-07-02 18:34:26 +0900

    feat: 로그인 또는 로그인하지 않은경우 Navbar 조건부렌더링 기능 추가 및 프로젝트 생성 페이지 경로추가

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+
+import { isShowingModalState, tokenState } from "../../recoil/atom";
+import Home from "../Home/Home";
+import Navbar from "../Navbar/Navbar";
+import RegisterProjectModal from "../RegisterProjectModal/RegisterProjectModal";
 import Topbar from "../Topbar/Topbar";
 
 export default function Layout() {
+  const token = useRecoilValue(tokenState);
+  const isShowingModal = useRecoilValue(isShowingModalState);
+
   return (
     <div>
       <Topbar />
+      {token ? <Navbar /> : "로그인 하세요"}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {isShowingModal && (
+          <Route path="/project" element={<RegisterProjectModal />} />
+        )}
+      </Routes>
     </div>
   );
 }
