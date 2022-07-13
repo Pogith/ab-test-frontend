2f7b45a6927df6acd842d5bf289caab0a1485f87
chlugsdh58 <chlgusdh58@gmail.com>
2022-07-13 21:39:35 +0900

    feat: ab test글자 클릭시 home 화면으로 이동 기능추가

--- src/component/Topbar/Topbar.js
+++ src/component/Topbar/Topbar.js
@@ -3,15 +3,24 @@ import classNames from "classnames/bind";
 
 import GoogleSignIn from "../GoogleSignIn/GoogleSignIn";
 import styles from "./Topbar.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 export default function Topbar() {
+  const navigate = useNavigate();
+
+  const handleHomeButtonClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className={cx("topbar")}>
       <div className={cx("topbar__wrapper")}>
         <div className={cx("topbar__left")}>
-          <div className={cx("topbar__logo")}>A/B test</div>
+          <div className={cx("topbar__logo")} onClick={handleHomeButtonClick}>
+            A/B test
+          </div>
         </div>
         <div className={cx("topbar__right")}>
           <GoogleSignIn />
