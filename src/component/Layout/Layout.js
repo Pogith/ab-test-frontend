79b1a808006cfb9236145b5c1d0d0a887f37049d
chlugsdh58 <chlgusdh58@gmail.com>
2022-07-13 15:22:14 +0900

    feat: screenshot 페이지 및 경로 추가 및 scss 코드 수정

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -3,10 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import classNames from "classnames/bind";
 
-import { tokenState } from "../../recoil/atom";
+import { isShowingScreenShotState } from "../../recoil/atom";
 import Home from "../Home/Home";
-import Sidebar from "../Sidebar/Sidebar";
 import RegisterProjectPage from "../RegisterProjectPage/RegisterProjectPage";
+import Sidebar from "../Sidebar/Sidebar";
+import ScreenShot from "../ScreenShot/ScreenShot";
 import ScriptKeyModal from "../ScriptKeyModal/ScriptKeyModal";
 import TestResultPage from "../TestResultPage/TestResultPage";
 import Topbar from "../Topbar/Topbar";
@@ -17,31 +18,41 @@ import styles from "./Layout.module.scss";
 const cx = classNames.bind(styles);
 
 export default function Layout() {
-  const token = useRecoilValue(tokenState);
+  const isShowingScreenShot = useRecoilValue(isShowingScreenShotState);
 
   return (
     <div>
-      <Topbar />
+      {!isShowingScreenShot && <Topbar />}
       <div className={cx("layout")}>
-        <div className={cx("layout__sidebar")}>
-          {token ? <Sidebar /> : "로그인 하세요"}
-        </div>
+        {!isShowingScreenShot && (
+          <div className={cx("layout__sidebar")}>
+            <Sidebar />
+          </div>
+        )}
         <div className={cx("layout__content")}>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/project" element={<RegisterProjectPage />} />
-            <Route path="/user/:uid" element={<UserPage />} />
+            {!isShowingScreenShot && (
+              <>
+                <Route path="/" element={<Home />} />
+                <Route path="/project" element={<RegisterProjectPage />} />
+                <Route path="/user/:uid" element={<UserPage />} />
+                <Route
+                  path="/user/project/:projectId"
+                  element={<UserProjectPage />}
+                />
+                <Route
+                  path="/user/project/:projectId/:uniqId"
+                  element={<ScriptKeyModal />}
+                />
+                <Route
+                  path="/user/project/result/:projectId"
+                  element={<TestResultPage />}
+                />
+              </>
+            )}
             <Route
-              path="/user/project/:projectId"
-              element={<UserProjectPage />}
-            />
-            <Route
-              path="/user/project/:projectId/:uniqId"
-              element={<ScriptKeyModal />}
-            />
-            <Route
-              path="/user/project/result/:projectId"
-              element={<TestResultPage />}
+              path="/user/project/:projectId/:uniqId/screenshot"
+              element={<ScreenShot />}
             />
           </Routes>
         </div>
