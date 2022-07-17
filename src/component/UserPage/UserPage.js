3ce2b60d8839406aa5e4a754dc110f0ea3c34b6b
chlugsdh58 <chlgusdh58@gmail.com>
2022-07-17 22:51:24 +0900

    refactor: axios 폴더 구조를 변경함에 따라 코드 수정

--- src/component/UserPage/UserPage.js
+++ src/component/UserPage/UserPage.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import classNames from "classnames/bind";
 
+import { axiosGetRequest, axiosDeleteRequest } from "../../data/api";
 import styles from "./UserPage.module.scss";
 
 const cx = classNames.bind(styles);
@@ -10,42 +10,33 @@ const cx = classNames.bind(styles);
 export default function UserPage() {
   const navigate = useNavigate();
   const [projects, setProjects] = useState(null);
-  const [isRendering, setIsRendering] = useState(false);
   const userUid = localStorage.getItem("user");
 
   useEffect(() => {
-    const fetchProjectsData = async () => {
-      try {
-        const response = await axios.get(
-          `${process.env.REACT_APP_SERVER_URL}/users/${userUid}/projects`,
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          }
-        );
+    fetchProjectData();
+  }, []);
 
-        setProjects(response);
-      } catch (err) {
-        console.error("Error", err);
-      }
-    };
+  const fetchProjectData = async () => {
+    try {
+      const projectDataGetRequestUrl = `${process.env.REACT_APP_SERVER_URL}/users/${userUid}/projects`;
+      const response = await axiosGetRequest(projectDataGetRequestUrl);
 
-    fetchProjectsData();
-  }, [isRendering]);
+      setProjects(response);
+    } catch (err) {
+      console.error("Error", err);
+    }
+  };
 
-  const handleProjectDeleteButtonClick = (projectId) => {
-    axios
-      .delete(
-        `${process.env.REACT_APP_SERVER_URL}/users/${userUid}/projects/${projectId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      )
-      .then(() => setIsRendering(!isRendering))
-      .catch((err) => console.error("Error", err));
+  const handleProjectDeleteButtonClick = async (projectId) => {
+    try {
+      const projectDeleteRequestUrl = `${process.env.REACT_APP_SERVER_URL}/users/${userUid}/projects/${projectId}`;
+
+      await axiosDeleteRequest(projectDeleteRequestUrl);
+
+      fetchProjectData();
+    } catch (err) {
+      console.error("Project Delete Error", err);
+    }
   };
 
   return (
