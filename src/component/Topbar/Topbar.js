326f1d197351d2f7d11f964433260d4207a7af0b
chlugsdh58 <chlgusdh58@gmail.com>
2022-07-19 15:13:52 +0900

    design: Sidebar 컴포넌트를 제거하고 Topbar에 페이지로 이동하는 기능 추가 및 scss 코드 수정

--- src/component/Topbar/Topbar.js
+++ src/component/Topbar/Topbar.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { BiWindows } from "react-icons/bi";
 import classNames from "classnames/bind";
 
+import { firebaseUserState } from "../../recoil/atom";
 import GoogleSignIn from "../GoogleSignIn/GoogleSignIn";
 import styles from "./Topbar.module.scss";
-import { useNavigate } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 export default function Topbar() {
   const navigate = useNavigate();
+  const userUid = useRecoilValue(firebaseUserState);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const location = useLocation();
+
+  const sidebarItems = [
+    {
+      text: "Home",
+      path: "/",
+      section: "",
+    },
+    {
+      text: "Register",
+      path: "/project",
+      section: "project",
+    },
+    {
+      text: "My Page",
+      path: `/user/${userUid}`,
+      section: "user",
+    },
+  ];
+
+  useEffect(() => {
+    const currentPath = window.location.pathname.split("/")[1];
+    const activeItem = sidebarItems.findIndex(
+      (item) => item.section === currentPath
+    );
+
+    setActiveIndex(currentPath.length === 0 ? 0 : activeItem);
+  }, [location]);
 
   const handleHomeButtonClick = () => {
     navigate("/");
@@ -19,11 +52,38 @@ export default function Topbar() {
       <div className={cx("topbar__wrapper")}>
         <div className={cx("topbar__left")}>
           <div className={cx("topbar__logo")} onClick={handleHomeButtonClick}>
-            A/B test
+            <BiWindows size="30px" />
+            <span className={cx("topbar__logo__title")}>A/B test</span>
           </div>
         </div>
-        <div className={cx("topbar__right")}>
-          <GoogleSignIn />
+        <div className={cx("topbar__menu")}>
+          {!userUid ? (
+            <div className={cx("topbar__menu__message")}>로그인해주세요</div>
+          ) : (
+            <>
+              {sidebarItems.map((item, index) => (
+                <Link
+                  className={cx("topbar__menu__link")}
+                  to={item.path}
+                  key={index}
+                >
+                  <div
+                    className={cx("topbar__menu__item")}
+                    style={{
+                      color: `${activeIndex === index ? "red" : "white"}`,
+                    }}
+                  >
+                    <div className={cx("topbar__menu__item__text")}>
+                      {item.text}
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </>
+          )}
+          <div className={cx("topbar__right")}>
+            <GoogleSignIn />
+          </div>
         </div>
       </div>
     </div>
